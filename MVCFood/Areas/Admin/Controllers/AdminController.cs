e1ffdf1c6519f3e98db424299d16e25438cbe9d8
huy <thangbom150895@yahoo.com>
2017-10-28 19:42:32 +0700

    Revert "comit lần 3 huy"
    This reverts commit d0457970888b7503ee10342621011ffd4c2ba008.

--- MVCFood/Areas/Admin/Controllers/AdminController.cs
+++ MVCFood/Areas/Admin/Controllers/AdminController.cs
@@ -13,7 +13,6 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using System.Security.Principal;
-using System.IO;
 
 namespace MVCFood.Areas.Admin.Controllers
 {
@@ -38,126 +37,14 @@ namespace MVCFood.Areas.Admin.Controllers
             return View(viewmodel);
         }
         [Authorize(Roles = "Admin")]
-        public ActionResult Products(int id)
-        {
-            var userid = User.Identity.GetUserId();
-            var users = _db.Users.Where(c => c.Id == userid).Single();
-            var viewmodel = new AdminViewModels
-            {
-                Users = users,
-                AllTypeProduct = _db.TypeProducts.ToList(),
-                AllProduct = _db.Products.Where(c => c.TypeProductId == id).ToList()
-            };
-            return View(viewmodel);
-        }
-        [Authorize(Roles = "Admin")]
         public ActionResult AddCategoryNew()
         {
-            var userid = User.Identity.GetUserId();
-            var users = _db.Users.Where(c => c.Id == userid).Single();
             List<TypeProduct> viewmodel = _db.TypeProducts.ToList();
             var viewmodels = new AdminViewModels
             {
-                Users = users,
-                AllTypeProduct = viewmodel
+                AllTypeProduct = viewmodel,
             };
             return View(viewmodels);
-        }
-        [Authorize(Roles = "Admin")]
-        public ActionResult CreateCategory()
-        {
-           
-            return View();
-        }
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult CreateCategory(TypeProduct typeProduct)
-        {
-            var categories = new TypeProduct
-            {
-                Name = typeProduct.Name
-            };
-            _db.TypeProducts.Add(categories);
-            _db.SaveChanges();
-            return RedirectToAction("AddCategoryNew");
-        }
-        [Authorize(Roles = "Admin")]
-        public ActionResult EditCategory(int id)
-        {
-            var categories = _db.TypeProducts.Single(c => c.Id == id);
-            var category = new TypeProduct
-            {
-                Id = categories.Id,
-                Description=categories.Description,
-                Name = categories.Name
-            };
-            return View("CreateCategory", category);
-        }
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult EditCategory(TypeProduct typeProduct)
-        {
-            var categories = _db.TypeProducts.Single(c => c.Id == typeProduct.Id);
-            categories.Id = typeProduct.Id;
-            categories.Description = typeProduct.Description;
-            categories.Name = typeProduct.Name;
-            _db.SaveChanges();
-            return RedirectToAction("AddCategoryNew");
-        }
-        [Authorize(Roles = "Admin")]
-        public ActionResult DeleteCategory(int id)
-        {
-            var categories = _db.TypeProducts.Single(c => c.Id == id);
-            _db.TypeProducts.Remove(categories);
-            _db.SaveChanges();
-            return RedirectToAction("AddCategoryNew");
-        }
-
-        [Authorize(Roles = "Admin")]
-        public ActionResult CreateProducts()
-        {
-            var viewmodel = new ViewModelProduct
-            {
-                AllTypeProduct = _db.TypeProducts.ToList()
-            };
-            return View(viewmodel);
-        }
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult CreateProducts(ViewModelProduct viewmodel, HttpPostedFileBase file)
-        {
-            if (file != null)
-            {
-                string pic = System.IO.Path.GetFileName(file.FileName);
-                string path = System.IO.Path.Combine(
-                                       Server.MapPath("~/Assets/Personal/image/"), pic);
-                // file is uploaded
-                file.SaveAs(path);
-
-                // save the image path path to the database or you can send image 
-                // directly to database
-                // in-case if you want to store byte[] ie. for DB
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    file.InputStream.CopyTo(ms);
-                    byte[] array = ms.GetBuffer();
-                }
-
-            }
-            var product = new Product
-            {
-                Name=viewmodel.Name,
-                Description=viewmodel.Description,
-                UnitPrice=viewmodel.UnitPrice,
-                PromotionPrice=viewmodel.PromotionPrice,
-                Image=viewmodel.Image,
-                DateEntered=viewmodel.DateEntered,
-                Quantum=viewmodel.Quantum,
-                TypeProductId=viewmodel.TypeProductId
-            };
-            _db.Products.Add(product);
-            _db.SaveChanges();
-            return RedirectToAction("Products", new { id = viewmodel.TypeProductId });
         }
     }
 }
